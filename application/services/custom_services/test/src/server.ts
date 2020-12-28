e9b6d75dbce1bb106855fb800f702e10f269fec4
ravikumarnr <ravikumarnr>
2020-12-28 10:19:40 +0000

    updated generated code

--- application/services/custom_services/test/src/server.ts
+++ application/services/custom_services/test/src/server.ts
@@ -5,7 +5,7 @@ import * as mongoose from 'mongoose';
 import * as cors from 'cors';
 import { WinstonLogger } from './config/WinstonLogger';
 
-const PORT = 8057;
+const PORT = 8059;
 
 class App {
 
